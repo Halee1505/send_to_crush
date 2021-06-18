1f896445dbe8b3ffa17f04c0987cf109bf23e3e2
Halee <danhauloc@gmail.com>
2021-06-18 16:47:10 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    caption__1 : "edit caption 1",
+    button__1: "edit submit",
+    main:"Edit main title",
+    sub:"Edit sub title",
+    yes_answer:"edit yes answer",
+    no_answer:"edit no answer",
+    text: "Nhập vòa thanh ở dưới nè",
+    send_text: "Bấm vào đây để gửi nè", 
+    link: "https://www.youtube.com/"
+    }
